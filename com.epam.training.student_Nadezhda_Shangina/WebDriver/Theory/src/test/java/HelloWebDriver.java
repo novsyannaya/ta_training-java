ca2d0f4ddb958be019f198bc21655d59e4d3d3d0
Nadezhda <novsyannaya@gmail.com>
2023-11-16 16:38:57 -0600

    just learning - Page Object

--- com.epam.training.student_Nadezhda_Shangina/WebDriver/Theory/src/test/java/HelloWebDriver.java
+++ com.epam.training.student_Nadezhda_Shangina/WebDriver/Theory/src/test/java/HelloWebDriver.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import wait.CustomConditions;
+import waits.CustomConditions;
 
 import java.time.Duration;
 import java.util.List;
--- com.epam.training.student_Nadezhda_Shangina/WebDriver/Theory/src/test/java/waits/WebDriverSyncExplicit.java
+++ com.epam.training.student_Nadezhda_Shangina/WebDriver/Theory/src/test/java/waits/WebDriverSyncExplicit.java
@@ -1,4 +1,4 @@
-package wait;
+package waits;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
