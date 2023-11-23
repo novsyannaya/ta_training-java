aa970c44b2fc8fe566695128430e88d90bebe191
Nadezhda <novsyannaya@gmail.com>
2023-11-23 17:29:14 -0600

    WebDriver_task1

--- com.epam.training.student_Nadezhda_Shangina/WebDriver/Task_1/src/test/java/test/CreatePasteTest.java
+++ com.epam.training.student_Nadezhda_Shangina/WebDriver/Task_1/src/test/java/test/CreatePasteTest.java
@@ -1,13 +1,31 @@
 package test;
 
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
 import page.HomePage;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CreatePasteTest {
+    HomePage homePage;
+    ChromeDriver driver;
+
+    @BeforeClass
+    public void beforeTest() {
+        driver = new ChromeDriver();
+        homePage = new HomePage(driver);
+    }
+
+    @AfterTest
+    public void endTest() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
     @Test
     public void CreatePasteCodeExpirationName() {
-        HomePage homePage=new HomePage(new ChromeDriver());
+        homePage=new HomePage(driver);
         homePage.openPage();
         homePage.setCode("Hello from WebDriver");
 //        homePage.setExpiration("10 Minutes");
