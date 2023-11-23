55718ce65c4b31f557fc727b3148e03212bd700c
Nadezhda <novsyannaya@gmail.com>
2023-11-23 13:41:51 -0600

    WebDriver_task1

---
+++ com.epam.training.student_Nadezhda_Shangina/WebDriver/Task_1/src/test/java/test/CreatePasteTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.testng.annotations.Test;
+import page.HomePage;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreatePasteTest {
+    @Test
+    public void CreatePasteCodeExpirationName() {
+        HomePage homePage=new HomePage(new ChromeDriver());
+        homePage.openPage();
+        homePage.setCode("Hello from WebDriver");
+//        homePage.setExpiration("10 Minutes");
+
+
+    }
+
+
+}
